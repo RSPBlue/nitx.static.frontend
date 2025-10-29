c48baea0cdf7f06cab3e0d064efc4d2e2f1e0811
Francisco Stenico <francisco.stenico@gmail.com>
2025-10-28 23:14:28 -0300

    STYLE(Deliver): Responsividade

--- components/Deliver/index.tsx
+++ components/Deliver/index.tsx
@@ -14,49 +14,51 @@ const Deliver = () => {
       <Title color="white">
         COMO <strong>ENTREGAMOS</strong>
       </Title>
-      <div className="flex justify-between w-full">
-        <ArcherContainer>
-          <div className="flex-1 flex flex-col gap-10 justify-center h-full">
-            <CardDeliver archerId="stroke-connector-start">
-              Entender bem as necessidades
-            </CardDeliver>
-            <CardDeliver>Definir o modelo</CardDeliver>
-            <CardDeliver>Tratativas (propostas e afins)</CardDeliver>
-            <CardDeliver>Início dos serviços e implementação</CardDeliver>
-            <CardDeliver>Apresentar, aprender e otimizar</CardDeliver>
+      <div className="flex justify-center lg:justify-between">
+        <ArcherContainer className="w-fit">
+          <div className="flex justify-center lg:justify-between">
+            <div className="lg:flex-1 w-full sm:w-2/3 flex flex-col gap-10 justify-center h-full">
+              <CardDeliver archerId="stroke-connector-start">
+                Entender bem as necessidades
+              </CardDeliver>
+              <CardDeliver>Definir o modelo</CardDeliver>
+              <CardDeliver>Tratativas (propostas e afins)</CardDeliver>
+              <CardDeliver>Início dos serviços e implementação</CardDeliver>
+              <CardDeliver>Apresentar, aprender e otimizar</CardDeliver>
 
-            <div className="flex gap-4 text-xl font-medium mt-10 w-2/3">
-              <ArcherElement
-                id="stroke-connector-end"
-                relations={[
-                  {
-                    targetId: `stroke-connector-start`,
-                    targetAnchor: "middle",
-                    sourceAnchor: "top",
-                    style: {
-                      strokeColor: "#f1f3f5",
-                      endMarker: false,
-                      strokeWidth: 2,
+              <div className="flex gap-4 text-xl font-medium mt-10 w-2/3">
+                <ArcherElement
+                  id="stroke-connector-end"
+                  relations={[
+                    {
+                      targetId: `stroke-connector-start`,
+                      targetAnchor: "middle",
+                      sourceAnchor: "top",
+                      style: {
+                        strokeColor: "#f1f3f5",
+                        endMarker: false,
+                        strokeWidth: 2,
+                      },
                     },
-                  },
-                ]}
-              >
-                <div>
-                  <Image
-                    src={AsteriskIcon}
-                    alt="Seta apontada para a diagonal superior direita"
-                    className="w-8 aspect-square"
-                  />
-                </div>
-              </ArcherElement>
-              <p className="px-1 w-1/3 text-background flex-1">
-                Pós: estar lado a lado para suportar, ampliar/melhorias
-                contínuas e escalar.
-              </p>
+                  ]}
+                >
+                  <div>
+                    <Image
+                      src={AsteriskIcon}
+                      alt="Seta apontada para a diagonal superior direita"
+                      className="w-8 aspect-square"
+                    />
+                  </div>
+                </ArcherElement>
+                <p className="px-1 w-1/3 text-background flex-1">
+                  Pós: estar lado a lado para suportar, ampliar/melhorias
+                  contínuas e escalar.
+                </p>
+              </div>
             </div>
           </div>
         </ArcherContainer>
-        <figure className="relative flex-1 flex justify-center min-w-[420px]">
+        <figure className="hidden relative flex-1 justify-center min-w-[420px] lg:flex">
           <Image src={NitxBuilding} alt="Prédio NITX" className="z-10" />
           <div className="absolute w-[420px] rounded-xl shadow-2xl -translate-y-[5%] bg-white h-9/10 top-[10%] z-0" />
         </figure>
